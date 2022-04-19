0d7ba218fcc3314d973160aa50a4918bb216c88b
Mosericko <mosericko@gmail.com>
2022-04-19 04:41:03 +0300

    port environment added

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const app = express();
 
 //mongo db uri
 const dbURI = process.env.MONGO_URI;
+const port = process.env.PORT || 5000;
 
 mongoose
   .connect(dbURI)
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(port))
   .catch((err) => console.log(err));
 
 // register view engine
